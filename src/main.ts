b1c2ffb22ea16f3deac59b2360c780e881096e60
min.bai <min.bai@bondee.com>
2025-04-25 15:39:21 +0800

    feat: 连接mysql

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 处理跨域
+  app.enableCors();
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('nest_server_port')!);
 }
 bootstrap();
